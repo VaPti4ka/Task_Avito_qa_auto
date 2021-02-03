df4db56032323eedd70f9ffc862d25f7e1c30ccb
VadimPti4ka <IvakinVS@gmail.com>
2021-02-03 05:27:47 +0300

    Рекурсивно вызываемые функции set_param_value и get_value_from_values Необходимо дописать пути выставления значений value: сейчас id из values.json приоритетнее значения из вложенного values

--- prog/main.py
+++ prog/main.py
@@ -24,12 +24,31 @@ class MakeStructure:
         self.result = self.testcase.copy()
         for parameter in self.result["params"]:
             if "values" in parameter.keys():
-                pass
+                parameter["value"] = self.get_value_from_values(parameter["id"], parameter['values'])
+                # print(parameter)
             else:
-                parameter["value"] = self.get_value_from_values(parameter["id"])
-                print("Параметр ->", parameter)
+                parameter["value"] = self.get_value_from_file(parameter["id"])
 
-    def get_value_from_values(self, elem_id):
+        self.make_result_file()
+
+    def get_value_from_values(self, par_id, values):
+        value = self.get_value_from_file(par_id)
+        value = values[0]['title'] if value == "" else value
+        # print(value)
+        if "params" in values[0]:
+            print(values[0])
+            self.set_param_value(values)
+        return value
+
+    def set_param_value(self, params):
+        for parameter in params:
+            if "values" in parameter.keys():
+                parameter["value"] = self.get_value_from_values(parameter["id"], parameter['values'])
+            else:
+                pass
+                parameter["value"] = self.get_value_from_file(parameter["id"])
+
+    def get_value_from_file(self, elem_id):
         for elem in self.values:
             if elem["id"] == elem_id:
                 return elem["value"]
