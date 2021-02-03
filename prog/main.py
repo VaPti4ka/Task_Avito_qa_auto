b807a32fe9f99818e9b7e619e992355939213782
VadimPti4ka <ivakinvs@gmail.com>
2021-02-03 07:19:07 +0300

    Решение готово, остались тесты))

--- prog/main.py
+++ prog/main.py
@@ -5,38 +5,42 @@ class MakeStructure:
     filepath_origin = "../data_files/original/"
     filepath_created = "../data_files/created/"
 
+
     def __init__(self):
-        self.setting = self.get_file_data(self.filepath_origin + "setting.json")
-        self.testcase = self.get_file_data(self.filepath_origin + self.setting["testcase_2"])
-        self.values = self.get_file_data(self.filepath_origin + self.setting["value"])["values"]
+        self.setting = self.get_data_from_file(self.filepath_origin + "setting.json")
+        self.testcase = self.get_data_from_file(self.filepath_origin + self.setting["testcase_2"])
+        self.values = self.get_data_from_file(self.filepath_origin + self.setting["value"])["values"]
         self.result = None
 
+
+    # Возвращает содержимое JSON файла
     @staticmethod
-    def get_file_data(filename):
-        with open(filename, encoding='utf-8') as file:
+    def get_data_from_file(filename):
+        with open(filename, "r", encoding='utf-8') as file:
             return json.load(file)
 
 
+    # Записываем исправленную структуру в файл. Если не было - создается
     def make_result_file(self):
         with open(self.filepath_created + "res_file.json", "w", encoding="utf-8") as file:
-            json.dump(self.result, file, indent=2)
+            json.dump(self.result, file, indent=2, ensure_ascii=False)
 
-
+    # Рекурсивная фун-я исправления структуры
     def make_structure(self):
         self.result = self.testcase.copy()
         self.set_param_value(self.result["params"])
         self.make_result_file()
 
-
+    # Проверяет текущий уровень на наличие вложенных values, записывает значение в value
     def set_param_value(self, params):
         for parameter in params:
             if "values" in parameter.keys():
-                pass
                 parameter["value"] = self.get_value_from_values(parameter["id"], parameter['values'])
             elif "value" in parameter.keys():
                 parameter["value"] = self.get_value_from_file(parameter["id"])
 
 
+    # Проверяет текущий уровень на наличие вложенных params, выбирает и возвращает значение values
     def get_value_from_values(self, par_id, values):
         val_id = self.get_value_from_file(par_id)
         value = ""
@@ -49,7 +53,7 @@ class MakeStructure:
             value = values[0]['title']
         return value
 
-
+    # Получает значение values из Values.json
     def get_value_from_file(self, elem_id):
         for elem in self.values:
             if elem["id"] == elem_id:
