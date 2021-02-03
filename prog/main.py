97f87c174b80819bca475d3e483a23e5ec23cd55
VadimPti4ka <IvakinVS@gmail.com>
2021-02-03 04:26:32 +0300

    В make_structure добавлена обработка базового случая, когда у параметра нет поля values

--- prog/main.py
+++ prog/main.py
@@ -8,17 +8,33 @@ class MakeStructure:
     def __init__(self):
         self.setting = self.get_file_data(self.filepath_origin + "setting.json")
         self.testcase = self.get_file_data(self.filepath_origin + self.setting["testcase_2"])
-        self.values = self.get_file_data(self.filepath_origin + self.setting["value"])
+        self.values = self.get_file_data(self.filepath_origin + self.setting["value"])["values"]
         self.result = None
 
     @staticmethod
     def get_file_data(filename):
-        with open(filename) as file:
+        with open(filename, encoding='utf-8') as file:
             return json.load(file)
 
     def make_result_file(self):
         with open(self.filepath_created + "res_file.json", "w", encoding="utf-8") as file:
             json.dump(self.result, file, indent=2)
 
+    def make_structure(self):
+        self.result = self.testcase.copy()
+        for parameter in self.result["params"]:
+            if "values" in parameter.keys():
+                pass
+            else:
+                parameter["value"] = self.get_value_from_values(parameter["id"])
+                print("Параметр ->", parameter)
+
+    def get_value_from_values(self, elem_id):
+        for elem in self.values:
+            if elem["id"] == elem_id:
+                return elem["value"]
+        return ""
+
 
 test_case = MakeStructure()
+test_case.make_structure()
