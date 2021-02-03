0db3d93115ff00046598c8125b22727a6ad21851
VadimPti4ka <IvakinVS@gmail.com>
2021-02-03 03:22:43 +0300

    Init commit
    Папки:
    1) Data_files - папка для хранения файлов для обработки. origin - для исходных файлов (настройки, значения, тесткейс)
    2) prog - собственно скрипт
    3) tests - сделаю отдельные тесты, думаю

---
+++ prog/main.py
@@ -0,0 +1,18 @@
+import json
+
+
+class MakeStructure:
+    filepath_origin = "../data_files/original/"
+    filepath_created = "../data_files/created/"
+
+    def __init__(self):
+        self.testcase = None
+        self.values = None
+        self.result = None
+        self.setting = None
+
+    def get_file_data(self, filename):
+        pass
+
+    def make_result_file(self):
+        pass
